56a75e4bccaa2707e0d14a8f38c8529ce5adace7
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-14 11:48:39 -0500

    Add a solution for maxChar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const stgObj = {};
+  str.split('').forEach(char => {
+    stgObj[char] = stgObj[char] + 1 || 1;
+  });
+  let maxChar = [1, ''];
+  for (const char in stgObj) {
+    if (stgObj[char] >= maxChar[0]) {
+      maxChar[0] = stgObj[char];
+      maxChar[1] = char;
+    }  
+  }
+  return maxChar[1];
+}
 
 module.exports = maxChar;
